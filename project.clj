7fff2673eb2e7a8676c2cfbac167fb48084c35e6
Tim Greene <tim.g.greene@gmail.com>
2017-10-22 14:00:00 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-yada/lein-template "0.1.0-SNAPSHOT"
+(defproject simple-yada/lein-template "0.1.0"
   :description "A very simple yada template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
